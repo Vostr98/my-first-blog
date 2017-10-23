43caa3687c10c91a31372d02b9a4fad887fbc734
Vostr <vostr9836@gmail.com>
2017-10-23 17:39:41 +0300

    My Django Project, first commit

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    KEK = 'keking'
+    LOL = 'loling'
+    HEH = 'hehing'
+    LMAO = 'lmaoing'
+    OR_CHOICES = (
+        (KEK,'kek'),
+        (LOL,'lol'),
+        (HEH,'heh'),
+        (LMAO,'lmao'),
+    )
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200, default = 'Title')
+    text = models.TextField(default = 'Text')
+    created_date = models.DateTimeField(default = timezone.now)
+    published_date = models.DateTimeField(blank = True, null = True)
+    question = models.CharField(max_length=14, choices=OR_CHOICES,default=KEK)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
